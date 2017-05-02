3868e72ac643d76e630a73505c5cc6ebe488a9be
Jon Willemain <jon.willemain@gmail.com>
2017-05-02 14:57:32 -0600

    created our public folder and moved the necessary files in that folder

--- server.js
+++ server.js
@@ -3,8 +3,17 @@ const app = express()
 const bodyParser = require('body-parser')
 const md5 = require('md5')
 
+app.use(express.static('public'))
+
 app.set('port', process.env.PORT || 3000)
+app.locals.title = 'Jet Fuel'
 
 app.get('/', (request, response) => {
-  response.send('hello')
+  fs.readFile(`${__dirname}/index.html`, (err, file) => {
+    response.send(file)
+  })
+})
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on ${app.get('port')}.`)
 })
