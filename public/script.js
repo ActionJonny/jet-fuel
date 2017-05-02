c24b71853ca6c49f2bed9980536473cf837ae261
Tim <tbrandle53@gmail.com>
2017-05-02 16:21:18 -0600

    can add new folder, post request to server, retrieve all folders and append on click of .current-folders button

--- public/script.js
+++ public/script.js
@@ -3,12 +3,42 @@ console.log($('.logo').text());
 
 
 $('.current-folder').on('click', function () {
+  retrieveAllFolders()
 $('.folder-section').show()
   // $('.folders').show("slide", "left" , 1000)
   $('.folder-div').show("slide")
+
 })
 
 $('.folder-section').on('click', '.close', function () {
   $('.folder-section').hide()
+})
+
+
+const addNewFolder = (title) => {
+  fetch('/folders', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ title })
+  })
+  .then(response => response.json())
+  .then(json => $('.folder-div').append(`<div>${json.title}</div>`))
+}
+
+const appendFolders = (json) => {
+  json.length && json.map(object => $('.folder-div').append(`<div>${object.title}</div>`))
+}
+
+const retrieveAllFolders = () => {
+
+  fetch('/folders')
+    .then(response => response.json())
+    .then(json => appendFolders(json))
+}
+
+$('.folder-section').on('click', '.add-icon', function () {
+  const title = $('.new-folder').val()
+
+  addNewFolder(title)
 
 })
