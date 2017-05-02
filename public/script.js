01c472ed5c18b29cc4c5feca44053fb23cc82b57
Tim <tbrandle53@gmail.com>
2017-05-02 17:01:55 -0600

    Making get request and displaying folders...currently creating duplicates on folders-div

--- public/script.js
+++ public/script.js
@@ -4,9 +4,9 @@ console.log($('.logo').text());
 
 $('.current-folder').on('click', function () {
   retrieveAllFolders()
-$('.folder-section').show()
+  $('.folder-section').show()
   // $('.folders').show("slide", "left" , 1000)
-  $('.folder-div').show("slide")
+  // $('.folder-div').show("slide")
 
 })
 
@@ -22,15 +22,20 @@ const addNewFolder = (title) => {
     body: JSON.stringify({ title })
   })
   .then(response => response.json())
-  .then(json => $('.folder-div').append(`<div>${json.title}</div>`))
+  .then(json => $('.folder-div').append(`<div id=${json.id} class="folder">${json.title}</div>`))
 }
 
 const appendFolders = (json) => {
-  json.length && json.map(object => $('.folder-div').append(`<div>${object.title}</div>`))
+  json.length && json.map(object => {
+    $('.folder-div').append(`<div id=${object.id} class="folder">${object.title}</div>`)
+  })
 }
 
 const retrieveAllFolders = () => {
+  const foldersArray = $('.folder-div').find('.folder').attr('id')
 
+  // const folderIds = foldersArray.each(folder => console.log(folder))
+  // console.log(folderIds);
   fetch('/folders')
     .then(response => response.json())
     .then(json => appendFolders(json))
@@ -38,7 +43,6 @@ const retrieveAllFolders = () => {
 
 $('.folder-section').on('click', '.add-icon', function () {
   const title = $('.new-folder').val()
-
   addNewFolder(title)
-
+  $('.new-folder').val('')
 })
